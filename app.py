d85b49d45a98f273b002cf329ab88021cf36b0a8
Monika P <moniprem31@gmail.com>
2026-04-02 10:30:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -140,4 +140,5 @@ def seed():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
